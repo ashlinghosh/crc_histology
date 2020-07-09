c77059882c1bbe5f3d58997be33069f5d119ec19
GHA3KOR <Ashlin.Ghosh@in.bosch.com>
2020-07-09 17:20:50 +0530

    Created the project structure with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PyTorch implementation of Texture Classification in Colorectal Cancer Histology',
+    author='Ashlin Ghosh/Sreenath Jayachandran',
+    license='MIT',
+)
